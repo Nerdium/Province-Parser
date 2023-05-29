b9207c554d7f188cdcb7812df2de809c66570c22
nerdiumlol <1326536-nerdiumlol@users.noreply.replit.com>
2023-05-29 22:42:31 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+import json
+import numpy as np
+import trimesh
+from shapely import geometry
+import mapbox_earcut
+
+
+data = []
+
+with open("data.txt", "r") as inputFile:
+  data = json.loads(inputFile.read())
+  inputFile.close();
+
+
+print(data["features"][0])
+#coords = data["features"][1]["geometry"]["coordinates"][0]
+
+for i in range(len(data["features"])):
+  print(i)
+  if(data["features"][i]["geometry"]["type"] == "Polygon"):
+    coords = data["features"][i]["geometry"]["coordinates"][0]
+    poly = geometry.Polygon(coords)
+    mesh = trimesh.creation.extrude_polygon(poly, 1)
+    trimesh.exchange.export.export_mesh(mesh, "objs/mesh" + str(i) + ".obj")
